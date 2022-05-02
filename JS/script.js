1b89a362c47ee77f03a62d74c25791cd5017d2be
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-05-02 19:45:41 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function oddEven(){
+    var number = document.getElementById("input").value;
+
+        if(number %2==0){
+            alert("Even")
+        }
+        else if(number %3==0){
+            alert("Odd")
+        }
+        if
+
+}
